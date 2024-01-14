9c5b20dee374eb4940f1e16be75422f7e60e0218
Hugo-CASTELL <hugo.castell@outlook.fr>
2024-01-14 19:38:09 +0100

    Added Copilot config and cmp config

--- after/plugin/lsp_cmp.lua
+++ after/plugin/lsp_cmp.lua
@@ -15,6 +15,40 @@ lsp_zero.on_attach(function(client, bufnr)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+local cmp = require("cmp")
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+cmp.setup({
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+        },
+        {
+            { name = "buffer" },
+            { name = "path" },
+    }),
+})
+
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
+    }),
+})
+
 --- if you want to know more about lsp-zero and mason.nvim
 --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
